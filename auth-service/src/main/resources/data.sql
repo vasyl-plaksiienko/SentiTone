a241fd2af78a0b403d85b9b30cf8871a97798058
Vasyl Plaksiienko <vasyl.plaksiienko@gmail.com>
2025-10-02 23:17:04 +0200

    feat: apply liquibases when the service is starting [AI]

--- auth-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (username, password, enabled) VALUES
-  ('admin', '$2a$10$N9qo8uLOickgx2ZMRZo5i.Ur07XH8VEWil/92pT9bkHYOEqf9Wl16', TRUE);
-
-INSERT INTO authorities (username, authority) VALUES
-  ('admin', 'ROLE_USER'),
-  ('admin', 'ROLE_ADMIN');
